0c67fbd25dc8d600ec96dbd4229857f6272c941e
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2025-05-26 19:40:25 +0200

    add lock to sys script

--- scripts/.scripts/wofi-sys.sh
+++ scripts/.scripts/wofi-sys.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-case "$(printf "kill\nsuspend\nreboot\nshutdown" | wofi --dmenu -i -L 5)" in
+case "$(printf "kill\nsuspend\nreboot\nshutdown\nlock" | wofi --dmenu -i -L 6)" in
 	kill) ps -u $USER -o pid,comm,%cpu,%mem | wofi --dmenu -i -L 11 -p Kill: | awk '{print $1}' | xargs -r kill ;;
 	suspend) systemctl suspend -i ;;
 	reboot) systemctl reboot -i ;;
 	shutdown) shutdown now ;;
+    lock) pidof hyprlock || hyprlock ;;
 	*) exit 1 ;;
 esac
