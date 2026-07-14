4d50cb41d82255265037efa8b3f62d835402eaec
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2026-07-15 01:10:49 +0200

    remove unnecessary line

--- nvim/dot-config/nvim/lua/koopa/lazy/nimby.lua
+++ nvim/dot-config/nvim/lua/koopa/lazy/nimby.lua
@@ -1,7 +1,6 @@
 return {
 	dir = "/home/koopa/dev/github/nimby_lsp/editors/neovim/",
 	config = function(plugin)
-		vim.opt.rtp:append(plugin.dir .. "/editor/neovim")
 		require("nimbyscript").setup({
 			-- Optional: specify path to LSP binary
 			-- cmd = "/path/to/nimbyscript-lsp",
